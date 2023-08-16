e9f4e397687ffa14ea14b0e454727d6f1d55271a
Samuel Salazar <samsala32@gmail.com>
2023-08-15 20:53:25 -0500

    trabajo

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illuminate\View\View;
+
+class HomeController extends Controller{
+    public function index():view{
+        return view('home.index');
+    }
+}
